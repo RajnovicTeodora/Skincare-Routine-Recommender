130b69ce3e683a7f7773a646e6807fb6c529c0eb
Teodora Rajnovic <rajnovicteodora@gmail.com>
2022-06-17 17:46:59 +0200

    Update ProductRepository.java

--- drools-spring-app/src/main/java/sbnz/skincare/repository/ProductRepository.java
+++ drools-spring-app/src/main/java/sbnz/skincare/repository/ProductRepository.java
@@ -1,7 +1,15 @@
 package sbnz.skincare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import sbnz.skincare.facts.Product;
+import sbnz.skincare.facts.enumerations.ProductType;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByNameAndProductTypeAndManufacturer(@Param("name") String name,
+                                                              @Param("productType") ProductType productType,
+                                                              @Param("manufacturer") String manufacturer);
 }
